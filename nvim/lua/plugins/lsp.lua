f4a2a76c6bb9a8ddfd8a3395745e1f8063806c02
mist1351 <bloodmist@inbox.ru>
2023-06-07 10:23:38 +0600

    [feat] setup required lsp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -2,12 +2,51 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 -- Setup language servers.
 local lspconfig = require("lspconfig")
-lspconfig.pyright.setup({})
+-- vim-language-server
+lspconfig.vimls.setup({})
+-- cmake-language-server
+lspconfig.cmake.setup({})
+-- lua-language-server
+lspconfig.lua_ls.setup({
+	settings = {
+		Lua = {
+			runtime = {
+				-- Tell the language server which version of Lua you're using
+				-- (most likely LuaJIT in the case of Neovim)
+				version = "LuaJIT",
+			},
+			diagnostics = {
+				-- Get the language server to recognize the `vim` global
+				globals = {
+					"vim",
+					"require",
+				},
+			},
+			workspace = {
+				-- Make the server aware of Neovim runtime files
+				library = vim.api.nvim_get_runtime_file("", true),
+				checkThirdParty = false,
+			},
+			-- Do not send telemetry data containing a randomized but unique identifier
+			telemetry = {
+				enable = false,
+			},
+		},
+	},
+})
+-- vue-language-server
+lspconfig.volar.setup({})
+-- typescript-language-server
 lspconfig.tsserver.setup({})
-lspconfig.prismals.setup({})
+-- yaml-language-server
+lspconfig.yamlls.setup({})
+-- pyright
+lspconfig.pyright.setup({})
+-- css-lsp
 lspconfig.cssls.setup({ capabilities = capabilities })
-lspconfig.golangci_lint_ls.setup({})
+-- clangd
 lspconfig.clangd.setup({})
+-- rust-analyzer
 lspconfig.rust_analyzer.setup({
 	-- Server-specific settings. See `:help lspconfig-setup`
 	settings = {
@@ -40,15 +79,15 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
 		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
 		vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
-		-- vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-		-- vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-		-- vim.keymap.set('n', '<space>wl', function()
-		--   print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-		-- end, opts)
+		vim.keymap.set("n", "<leader>lwa", vim.lsp.buf.add_workspace_folder, opts)
+		vim.keymap.set("n", "<leader>lwr", vim.lsp.buf.remove_workspace_folder, opts)
+		vim.keymap.set("n", "<leader>lwl", function()
+			print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+		end, opts)
 		vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
-		vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, opts)
+		vim.keymap.set("n", "<leader>lR", vim.lsp.buf.rename, opts)
 		vim.keymap.set({ "n", "v" }, "<leader>la", vim.lsp.buf.code_action, opts)
-		-- vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+		vim.keymap.set("n", "<leader>lr", vim.lsp.buf.references, opts)
 		vim.keymap.set("n", "<leader>lf", function()
 			vim.lsp.buf.format({ async = true })
 		end, opts)
